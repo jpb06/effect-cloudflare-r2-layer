ca148797355874c3354cbd22c942e93956747f3e
jpb06 <jpb.06@outlook.com>
2024-10-16 20:53:20 +0200

    chore: exporting errors

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,4 @@ export type {
   BucketInfosInput,
   BucketInfosResult,
 } from './r2/implementations/bucket-infos.effect.js';
+export { FileStorageError, BucketNotFoundError } from './errors/index.js';
