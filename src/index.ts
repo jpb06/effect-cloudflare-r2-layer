f3246287ac01f3036f6173489fcd3c4c0b830c40
jpb06 <jpb.06@outlook.com>
2025-09-06 20:59:32 +0200

    chore: pinning actions deps (#307)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import type { FileStorage } from './layer/file-storage.layer.js';
@@ -6,15 +7,16 @@ import { FileStorageLayer } from './layer/file-storage.layer.js';
 
 export { FileStorageLayer };
 export type { FileStorage };
-export * from './r2/r2-file-storage.layer.js';
 
 export type {
-  PutObjectCommandOutput,
   CreateBucketCommandInput,
   CreateBucketCommandOutput,
+  PutObjectCommandOutput,
 } from '@aws-sdk/client-s3';
+
+export { BucketNotFoundError, FileStorageError } from './errors/index.js';
 export type {
   BucketInfosInput,
   BucketInfosResult,
 } from './r2/implementations/bucket-infos.effect.js';
-export { FileStorageError, BucketNotFoundError } from './errors/index.js';
+export * from './r2/r2-file-storage.layer.js';
